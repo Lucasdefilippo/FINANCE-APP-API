ccc4e2ba439a8a36777ca6866014516d8d803aed
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-07 12:02:27 -0300

    feat: add init migraton to create users and transaction tabele

---
+++ src/db/postgres/migration/01-init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS users(
+    ID UUID PRIMARY KEY,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+DO $$
+BEGIN 
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'transaction_type') THEN
+        CREATE TYPE TRANSACTION_TYPE AS ENUM ('EARNING', 'EXPENSE', 'INVESTMENT');
+    END IF;
+END $$;
+
+CREATE TABLE IF NOT EXISTS transactions(
+    ID UUID PRIMARY KEY,
+    user_id UUID REFERENCES users(ID) ON DELETE CASCADE,
+    amount DECIMAL(10, 2) NOT NULL,
+    transaction_date DATE NOT NULL,
+    type TRANSACTION_TYPE NOT NULL
+);
