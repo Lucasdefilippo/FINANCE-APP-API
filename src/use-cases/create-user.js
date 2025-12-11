875c5ad2229bd3cfe3141f1c122a6db25ab1e284
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-11 20:41:43 -0300

    feat: Implement email uniqueness check in CreateUserUseCase

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,11 +1,22 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+import { PostgresGetUserByEmail } from '../repositories/postgres/get-user-by-email.js'
 
 export class CreateUserUseCase {
     async execute(CreateUserParams) {
         const UserID = uuidv4()
         const HashedPassword = bcrypt.hashSync(CreateUserParams.password, 10)
+
+        const postgresGetUserByEmail = new PostgresGetUserByEmail()
+
+        const userWithProvideEmail = postgresGetUserByEmail.execute(
+            CreateUserParams.email,
+        )
+
+        if (userWithProvideEmail) {
+            throw new Error('The provide email is already in use.')
+        }
 
         const NewUser = {
             ...CreateUserParams,
