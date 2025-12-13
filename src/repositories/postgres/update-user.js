e9d3e7692570e1d42ee1e8a1e56ec05df4f896ac
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-13 17:31:37 -0300

    feat: Add PostgresUpdateUser

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,21 @@
+import { postgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresUpdateUser {
+    async execute(userId, userUpdateParams) {
+        const updateField = []
+        const updateValues = []
+
+        Object.keys(userUpdateParams).forEach((key) => {
+            updateField.push(`${key} = $${updateField.length + 1}`)
+            updateValues.push(userUpdateParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `UPDATE users SET ${updateField} WHERE id = ${updateValues.length} RETURNING *`
+
+        const updateUser = await postgresHelper(updateQuery, updateValues)
+
+        return updateUser
+    }
+}
