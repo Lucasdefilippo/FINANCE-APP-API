737bb308dd46af243e19587c70f96e8500369751
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-14 11:21:09 -0300

    feat Add DeleteUserRepository

---
+++ src/repositories/postgres/delete-user.js
@@ -0,0 +1,12 @@
+import { postgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresDeleteUserRepository {
+    async execute(userId) {
+        const result = await postgresHelper.query(
+            'DELETE FROM users WHERE id = $1 RETURNING *',
+            [userId],
+        )
+
+        return result
+    }
+}
