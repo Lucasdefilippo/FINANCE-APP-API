3936add2a6736f214033b909b90cb00cf9936af2
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-15 20:30:52 -0300

    feat: Add PostgresCreateTransactionRepository

---
+++ src/repositories/postgres/transactions/create-transaction.js
@@ -0,0 +1,17 @@
+import { postgresHelper } from '../../../db/postgres/helper'
+
+export class PostgresCreateTransactionRepository {
+    async execute(createTransactionParams) {
+        const result = await postgresHelper.query(
+            'INSERT INTO transactions (ID, user_id, amount, transaction_date, TRANSACTION_TYPE) VALUES ($1, $2, $3, $4, $5, $6)',
+            [
+                createTransactionParams.id,
+                createTransactionParams.user_id,
+                createTransactionParams.amount,
+                createTransactionParams.transaction_date,
+                createTransactionParams.TRANSACTION_TYPE,
+            ],
+        )
+        return result
+    }
+}
