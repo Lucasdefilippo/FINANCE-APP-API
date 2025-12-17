dc44567c50982840713d125fcbcd87acbff059dc
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-17 20:06:36 -0300

    feat: Implement user management controllers for create, delete, get by ID, and update operations

---
+++ src/controllers/user/get-user-by-id.js
@@ -0,0 +1,35 @@
+import { errorServer, notFound, Ok } from '../helpers/http.js'
+import { sendInvalidUserIdError, verifyUserId } from '../helpers/user.js'
+
+export class GetUserByIdController {
+    constructor(getUserByIdUseCase) {
+        this.getUserByIdUseCase = getUserByIdUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const isValidId = verifyUserId(httpRequest.params.userId)
+
+            if (!isValidId) {
+                return sendInvalidUserIdError()
+            }
+
+            const user = await this.getUserByIdUseCase.execute(
+                httpRequest.params.userId,
+            )
+
+            if (!user) {
+                return notFound({
+                    message: 'User Not found.',
+                })
+            }
+
+            return Ok({
+                user,
+            })
+        } catch (error) {
+            console.error(error)
+            return errorServer()
+        }
+    }
+}
