1ef527c222f0d2f47150bd0de2feb892285c6631
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-20 13:20:14 -0300

    feat: Add UpdateTransactionRepository

---
+++ src/repositories/postgres/transactions/update-transaction.js
@@ -0,0 +1,19 @@
+export class UpdateTransactionRepository {
+    async execute(userId, transactionUpdateParams) {
+        const updateField = []
+        const updateValues = []
+
+        Object.keys(transactionUpdateParams).forEach((key) => {
+            updateField.push(`${key} = $${updateField.length + 1}`)
+            updateValues.push(transactionUpdateParams[key])
+        })
+
+        updateValues.push(`${userId}`)
+
+        const updateQuery = `UPDATE users SET ${updateField} WHERE id = $${updateValues.length} RETURNING *`
+
+        const result = await postgresHelper.query(updateQuery, updateValues)
+
+        return result.rows[0]
+    }
+}
