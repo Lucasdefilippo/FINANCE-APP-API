2386e5c0c29660d8f9a5658757998cadd867533d
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-21 15:20:43 -0300

    feat: Add DeleteTransactionUseCase and export from index.js

---
+++ src/use-cases/transaction/delete-transaction.js
@@ -0,0 +1,12 @@
+export class DeleteTransactionUseCase {
+    constructor(deleteTransactionRepository) {
+        this.deleteTransactionRepository = deleteTransactionRepository
+    }
+
+    async execute(transactionId) {
+        const result =
+            await this.deleteTransactionRepository.execute(transactionId)
+
+        return result
+    }
+}
