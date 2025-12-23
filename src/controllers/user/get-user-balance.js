ee0a6d4d63ae47999123c688f87a722ad621ecec
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-23 06:50:29 -0300

    feat: Add GetUserBalanceController

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,38 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import {
+    checkIdIsValid,
+    invalidIdResponse,
+    badRequest,
+    errorServer,
+    Ok,
+} from '../helpers/index.js'
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const user = httpRequest.params.userId
+
+            const isValid = checkIdIsValid(user)
+
+            if (!isValid) {
+                return invalidIdResponse()
+            }
+
+            const result = await this.getUserBalanceUseCase.execute(user)
+
+            return Ok({ result })
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return badRequest({ message: 'User not found' })
+            }
+
+            console.error(error)
+
+            return errorServer
+        }
+    }
+}
