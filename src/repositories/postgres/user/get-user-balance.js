5e6a41e33b2393b7ce42253da87626b08577cab8
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-23 07:21:36 -0300

    fix: await asynchronous query in PostgresGetUserBalanceRepository

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -2,24 +2,20 @@ import { postgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
-        const result = postgresHelper.query(
+        const result = await postgresHelper.query(
             `
-                SELECT 
-                    SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS EARNING, 
-                    SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS EXPENSE, 
-                    SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS INVESTMENT,
-                    SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END - 
-                    CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END - 
-                    CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END
-                    ) AS BALANCE, 
-                FROM transactions 
-                WHERE user_id = '$1'
-            `,
+                    SELECT 
+                        SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earning, 
+                        SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expense, 
+                        SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investment,
+                        SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
+                        - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
+                        - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS balance
+                    FROM transactions 
+                    WHERE user_id = $1
+                `,
             [userId],
         )
-        return {
-            userId,
-            ...result.rows[0],
-        }
+        return { userId, ...result.rows[0] }
     }
 }
