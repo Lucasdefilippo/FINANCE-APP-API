412339d95f3eb5ef21d40d6df7083b4a14c3815b
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-28 15:37:23 -0300

    feat: add prisma to update user

--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,21 +1,12 @@
-import { postgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateUser {
     async execute(userId, userUpdateParams) {
-        const updateField = []
-        const updateValues = []
-
-        Object.keys(userUpdateParams).forEach((key) => {
-            updateField.push(`${key} = $${updateField.length + 1}`)
-            updateValues.push(userUpdateParams[key])
+        return await prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: userUpdateParams,
         })
-
-        updateValues.push(`${userId}`)
-
-        const updateQuery = `UPDATE users SET ${updateField} WHERE id = $${updateValues.length} RETURNING *`
-
-        const updateUser = await postgresHelper.query(updateQuery, updateValues)
-
-        return updateUser.rows[0]
     }
 }
