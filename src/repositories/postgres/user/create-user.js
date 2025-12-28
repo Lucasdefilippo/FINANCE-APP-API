a5bbd3522affb018391e9c1076f90f7393fc8c73
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-28 15:25:40 -0300

    feat: add prisma to create user

--- src/repositories/postgres/user/create-user.js
+++ src/repositories/postgres/user/create-user.js
@@ -1,17 +1,16 @@
-import { postgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
-        const result = await postgresHelper.query(
-            'INSERT INTO users (ID, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [
-                createUserParams.ID,
-                createUserParams.first_name,
-                createUserParams.last_name,
-                createUserParams.email,
-                createUserParams.password,
-            ],
-        )
-        return result.rows[0]
+        const result = await prisma.user.create({
+            data: {
+                id: createUserParams.iD,
+                first_name: createUserParams.first_name,
+                last_name: createUserParams.last_name,
+                email: createUserParams.email,
+                password: createUserParams.password,
+            },
+        })
+        return result
     }
 }
