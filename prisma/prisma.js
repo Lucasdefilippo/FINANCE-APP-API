3b396c04eb7d73f16c6e9f7604b0907189d78440
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-28 15:24:53 -0300

    fix: corrected routes and schedules

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,2 +1,17 @@
-import { PrismaClient } from './prisma/generated/client'
-export const prisma = new PrismaClient()
+import 'dotenv/config'
+import pkg from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
+
+const { PrismaClient } = pkg
+const { Pool } = pg
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
+export const prisma = new PrismaClient({
+    adapter,
+})
