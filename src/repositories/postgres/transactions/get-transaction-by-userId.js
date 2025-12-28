4344841a87272211d5d0cea5486bedf53862cfd8
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-28 16:29:19 -0300

    feat: add prisma to get transactions by userId

--- src/repositories/postgres/transactions/get-transaction-by-userId.js
+++ src/repositories/postgres/transactions/get-transaction-by-userId.js
@@ -1,11 +1,11 @@
-import { postgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetTransactionsByUserIdRepository {
     async execute(userId) {
-        const result = await postgresHelper.query(
-            'SELECT * FROM transactions WHERE user_id = $1',
-            [userId],
-        )
-        return result.rows
+        return await prisma.transaction.findMany({
+            where: {
+                user_id: userId,
+            },
+        })
     }
 }
