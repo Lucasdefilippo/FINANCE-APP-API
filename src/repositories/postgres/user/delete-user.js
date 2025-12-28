3e745c5db5f079a5f827d7a958d546e9fcc02086
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-28 15:36:19 -0300

    feat: add prisma to delete user

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,12 +1,16 @@
-import { postgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
-        const result = await postgresHelper.query(
-            'DELETE FROM users WHERE id = $1 RETURNING *',
-            [userId],
-        )
-
-        return result.rows[0]
+        try {
+            return await prisma.user.delete({
+                where: {
+                    id: userId,
+                },
+            })
+        } catch (err) {
+            console.error(err)
+            return null
+        }
     }
 }
