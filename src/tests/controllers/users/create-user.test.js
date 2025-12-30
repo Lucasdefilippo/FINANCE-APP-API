13a4e010a0c7e13450e0ad5eaad8aab0da7fa041
Lucas Defilippo <lucasdefi00@gmail.com>
2025-12-30 20:49:51 -0300

    test: add test to createUserController verify satusCode 400 if first_name is null or undefined

--- src/tests/controllers/users/create-user.test.js
+++ src/tests/controllers/users/create-user.test.js
@@ -1,13 +1,13 @@
 import { CreateUserController } from '../../../controllers/user/create-user'
 
 describe('Create User Controller', () => {
-    class CreateUserUseCase {
+    class CreateUserUseCaseStub {
         execute(user) {
             return user
         }
     }
     it('should create an user ', async () => {
-        const createUserUseCase = new CreateUserUseCase()
+        const createUserUseCase = new CreateUserUseCaseStub()
         const createUserController = new CreateUserController(createUserUseCase)
 
         const httpRequest = {
@@ -24,5 +24,22 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).not.toBeNull()
         expect(result.body).not.toBeUndefined()
+    })
+
+    it('should error 400 if firs_name is undefined or null', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                last_name: 'Defilippo',
+                email: 'lucas@defilippo.com',
+                password: '123456789',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
     })
 })
