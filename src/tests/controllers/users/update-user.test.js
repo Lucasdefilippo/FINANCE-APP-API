36e0f60264fd8e4e425a2690fc29478034e43b58
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-04 17:01:12 -0300

    test: ensure UpdateUserController returns 500 when UpdateUserUseCase throws a generic error

--- src/tests/controllers/users/update-user.test.js
+++ src/tests/controllers/users/update-user.test.js
@@ -90,4 +90,15 @@ describe('UpdateUserController', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 500 if UpdateUserUseCase throws generic error', async () => {
+        const { sut, updateUserUseCase } = makeSut()
+        jest.spyOn(updateUserUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const result = await sut.execute(httpRequest)
+
+        expect(result.statusCode).toBe(500)
+    })
 })
