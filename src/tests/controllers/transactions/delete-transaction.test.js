f6e6456f68022edda9a8721072ce608b12cebaf3
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-05 20:40:15 -0300

    test: ensure DeleteTransactionController returns 500 if DeleteTransactionUseCase throw generic error

--- src/tests/controllers/transactions/delete-transaction.test.js
+++ src/tests/controllers/transactions/delete-transaction.test.js
@@ -44,7 +44,7 @@ describe('DeleteTransactionController', () => {
         expect(result.statusCode).toBe(400)
     })
 
-    it('', async () => {
+    it('should returns 404 if transaction is not found', async () => {
         const { sut, deleteTransactionUseCase } = makeSut()
         jest.spyOn(deleteTransactionUseCase, 'execute').mockImplementationOnce(
             () => {
@@ -55,5 +55,16 @@ describe('DeleteTransactionController', () => {
         const result = await sut.execute(httpRequest)
 
         expect(result.statusCode).toBe(404)
+    })
+
+    it('should returns 500 if DeleteTransactionUseCase throws generic error', async () => {
+        const { sut, deleteTransactionUseCase } = makeSut()
+        jest.spyOn(deleteTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const result = await sut.execute(httpRequest)
+
+        expect(result.statusCode).toBe(500)
     })
 })
