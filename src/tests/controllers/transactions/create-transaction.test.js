b3caad8e6839b448e5912d9bba5cca8c923c4979
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-05 14:24:56 -0300

    test: ensure CreateTransactionController returns 500 if createTransactionUseCase throw generic error

--- src/tests/controllers/transactions/create-transaction.test.js
+++ src/tests/controllers/transactions/create-transaction.test.js
@@ -77,4 +77,15 @@ describe('CreateTransactionController', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 500 if createTransactionUseCase throw generic error', async () => {
+        const { sut, createTransactionUseCase } = makeSut()
+        jest.spyOn(createTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const result = await sut.execute(httpRequest)
+
+        expect(result.statusCode).toBe(500)
+    })
 })
