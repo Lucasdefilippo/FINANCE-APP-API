1317ceb8ee6a267a313452bfc1f21531cf9d883c
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-05 14:09:55 -0300

    test: ensure CreateTransactionController returns 400 if name is not valid

--- src/tests/controllers/transactions/create-transaction.test.js
+++ src/tests/controllers/transactions/create-transaction.test.js
@@ -44,4 +44,15 @@ describe('CreateTransactionController', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if name is not valid', async () => {
+        const { sut } = makeSut()
+        //act
+        const result = await sut.execute({
+            ...httpRequest.body,
+            name: undefined,
+        })
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
