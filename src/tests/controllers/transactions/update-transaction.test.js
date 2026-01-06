203f93f80117b5c078cb81ea2b653b2aac12700c
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-05 23:01:36 -0300

    test: ensure UpdateTransactinController return 400 if user_id provided is not valid

--- src/tests/controllers/transactions/update-transaction.test.js
+++ src/tests/controllers/transactions/update-transaction.test.js
@@ -55,4 +55,18 @@ describe('UpdateTransactionController', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if user_id provided is not valid', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({
+            params: { ...httpRequest.params },
+            body: {
+                ...httpRequest.body,
+                user_id: 'Invalid_UserId',
+            },
+        })
+
+        expect(result.statusCode).toBe(400)
+    })
 })
