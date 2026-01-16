1389f8e970bcdab489702bcaf284354c34f84779
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-15 22:19:53 -0300

    test: ensure get transactions by userId with successfully

---
+++ src/tests/use-cases/transactions/get-transaction-by-userId.test.js
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker'
+import { GetTransactionsByUserIdUseCase } from '../../../use-cases/transaction/get-transaction-by-userId'
+
+describe('Get Transaction By User Id', () => {
+    const userId = faker.string.uuid()
+
+    class GetTransactionByUserIdRepositoryStub {
+        async execute() {
+            return []
+        }
+    }
+
+    class GetUserByIdRepositoryStub {
+        async execute() {
+            return userId
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionsbyUserIdRepository =
+            new GetTransactionByUserIdRepositoryStub()
+        const getUserByIdRepository = new GetUserByIdRepositoryStub()
+
+        const sut = new GetTransactionsByUserIdUseCase(
+            getTransactionsbyUserIdRepository,
+            getUserByIdRepository,
+        )
+
+        return { sut, getTransactionsbyUserIdRepository, getUserByIdRepository }
+    }
+
+    it('should get transactions by userId with successfully', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(userId)
+
+        expect(result).toEqual([])
+    })
+})
