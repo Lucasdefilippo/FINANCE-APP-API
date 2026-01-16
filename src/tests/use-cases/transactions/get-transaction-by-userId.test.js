3820bd2e48a191053578092566cf9a80b134a5cc
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-15 22:23:49 -0300

    test: ensure GetUserByIdRepository throw UserNotFoundError if user does not exist

--- src/tests/use-cases/transactions/get-transaction-by-userId.test.js
+++ src/tests/use-cases/transactions/get-transaction-by-userId.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { GetTransactionsByUserIdUseCase } from '../../../use-cases/transaction/get-transaction-by-userId'
+import { UserNotFoundError } from '../../../errors/user'
 
 describe('Get Transaction By User Id', () => {
     const userId = faker.string.uuid()
@@ -35,5 +36,16 @@ describe('Get Transaction By User Id', () => {
         const result = await sut.execute(userId)
 
         expect(result).toEqual([])
+    })
+
+    it('should throw UserNotFoundError if user does not exist', async () => {
+        const { sut, getUserByIdRepository } = makeSut()
+        jest.spyOn(getUserByIdRepository, 'execute').mockRejectedValueOnce(
+            new UserNotFoundError(),
+        )
+
+        const promise = sut.execute(userId)
+
+        await expect(promise).rejects.toThrow()
     })
 })
