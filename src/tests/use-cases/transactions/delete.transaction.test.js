9ff89f2ba5f829cde01dddaafca2e10b38ec8ead
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-15 21:56:58 -0300

    test: ensure DeleteTransactionUseCase delete transaction with successfully

---
+++ src/tests/use-cases/transactions/delete.transaction.test.js
@@ -0,0 +1,40 @@
+import { faker } from '@faker-js/faker'
+import { DeleteTransactionUseCase } from '../../../use-cases/transaction/delete-transaction'
+
+describe('Delete Transaction Use Case', () => {
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: faker.string.uuid(),
+        name: faker.finance.currencyName(),
+        date: faker.date.anytime().toISOString(),
+        amount: Number(faker.finance.amount()),
+        type: 'EARNING',
+    }
+
+    const transactionId = faker.string.uuid()
+
+    class DeleteTransactionRepositoryStub {
+        async execute(transactionId) {
+            return {
+                ...transaction,
+                id: transactionId,
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const deleteTransactionRepository =
+            new DeleteTransactionRepositoryStub()
+        const sut = new DeleteTransactionUseCase(deleteTransactionRepository)
+
+        return { sut, deleteTransactionRepository }
+    }
+
+    it('should delete transaction with successfully', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(transactionId)
+
+        expect(result).toEqual({ ...transaction, id: transactionId })
+    })
+})
