390d15002fee73e4935066d3c3ea4f68a80d1ff1
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-18 21:28:06 -0300

    fix: ensure the standard in the id

--- src/tests/fixtures/user.js
+++ src/tests/fixtures/user.js
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 
 export const user = {
-    ID: faker.string.uuid(),
+    id: faker.string.uuid(),
     first_name: faker.person.firstName(),
     last_name: faker.person.lastName(),
     email: faker.internet.email(),
