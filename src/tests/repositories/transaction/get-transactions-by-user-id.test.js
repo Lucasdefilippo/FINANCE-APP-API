bc49192bef72e14f30735719a8c5ac5211b73a41
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-21 14:44:39 -0300

    test: ensure PostgresGetTransactionsByUserIdRepository throw if Prisma throws

--- src/tests/repositories/transaction/get-transactions-by-user-id.test.js
+++ src/tests/repositories/transaction/get-transactions-by-user-id.test.js
@@ -38,4 +38,15 @@ describe('Get Transactions By User Id Repository', () => {
             where: { user_id: user.id },
         })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresGetTransactionsByUserIdRepository()
+        jest.spyOn(prisma.transaction, 'findMany').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const promise = sut.execute(user.id)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
