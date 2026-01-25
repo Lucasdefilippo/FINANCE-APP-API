06522af020f190051606eef5ddc63fc14147d165
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-24 22:00:13 -0300

    test: ensure GET /api/transaction  returns 200 when fetching transactions successfully

--- src/tests/routes/transactions.e2e.test.js
+++ src/tests/routes/transactions.e2e.test.js
@@ -16,4 +16,22 @@ describe('Transactions Routes E2E test', () => {
 
         expect(response.status).toBe(201)
     })
+
+    it('GET /api/transaction should returns 200 when fetching transactions successfully', async () => {
+        await prisma.user.create({ data: user })
+        const { body: createdtransaction } = await request(app)
+            .post(`/api/transaction`)
+            .send({
+                ...transaction,
+                user_id: user.id,
+                id: undefined,
+            })
+
+        const response = await request(app).get(
+            `/api/transaction?userId=${createdtransaction.user_id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body[0].id).toBe(createdtransaction.id)
+    })
 })
