7b414466b44b1c5cca7f3190007e4e5e58340430
Lucas Defilippo <lucasdefi00@gmail.com>
2026-01-24 22:41:50 -0300

    feat: ensure PostgresUpdateTransactionRepository returns TransactionNotFoundError if transaction is not found

--- src/repositories/postgres/transactions/update-transaction.js
+++ src/repositories/postgres/transactions/update-transaction.js
@@ -1,12 +1,26 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client'
 import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/transaction.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, transactionUpdateParams) {
-        return await prisma.transaction.update({
-            where: {
-                id: transactionId,
-            },
-            data: transactionUpdateParams,
-        })
+        try {
+            return await prisma.transaction.update({
+                where: {
+                    id: transactionId,
+                },
+                data: transactionUpdateParams,
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                const code = error.code
+
+                if (code === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId)
+                }
+            }
+
+            throw error
+        }
     }
 }
