c2ce6fa0071bf8ba255febe8e1c6b4ef41cb49dd
Lucas Defilippo <lucasdefi00@gmail.com>
2026-02-05 14:15:04 -0300

    refac/test: ensure tests with auth middleware

--- src/tests/use-cases/transactions/update-transaction.test.js
+++ src/tests/use-cases/transactions/update-transaction.test.js
@@ -11,10 +11,21 @@ describe('Update Transaction Use Case', () => {
         }
     }
 
+    class GetTransactionByIdRepositoryStub {
+        async execute() {
+            return transaction
+        }
+    }
+
     const makeSut = () => {
         const updateTransactionRepository =
             new UpdateTransactionRepositoryStub()
-        const sut = new UpdateTransactionUseCase(updateTransactionRepository)
+        const getTransactionByIdRepository =
+            new GetTransactionByIdRepositoryStub()
+        const sut = new UpdateTransactionUseCase(
+            updateTransactionRepository,
+            getTransactionByIdRepository,
+        )
 
         return { sut, updateTransactionRepository }
     }
