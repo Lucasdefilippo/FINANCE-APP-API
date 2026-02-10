dab3db6b70dfd826205e58a26b4cb1a204684618
Lucas Defilippo <lucasdefi00@gmail.com>
2026-02-09 23:55:22 -0300

    feat: add from and to in use case

--- src/use-cases/transaction/get-transaction-by-userId.js
+++ src/use-cases/transaction/get-transaction-by-userId.js
@@ -6,15 +6,18 @@ export class GetTransactionsByUserIdUseCase {
             getTransactionsbyUserIdRepository
         this.getUserByIdRepository = getUserByIdRepository
     }
-    async execute(userId) {
+    async execute(userId, from, to) {
         const userValidate = await this.getUserByIdRepository.execute(userId)
 
         if (!userValidate) {
             throw new UserNotFoundError(userId)
         }
 
-        const result =
-            await this.getTransactionsbyUserIdRepository.execute(userId)
+        const result = await this.getTransactionsbyUserIdRepository.execute(
+            userId,
+            from,
+            to,
+        )
 
         return result
     }
