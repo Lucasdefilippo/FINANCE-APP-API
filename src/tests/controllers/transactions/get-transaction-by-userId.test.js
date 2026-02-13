b6e262e5e4b098dca7b36141c91ca365dcd5ddfc
Lucas Defilippo <lucasdefi00@gmail.com>
2026-02-13 14:19:34 -0300

    test: ensure GetTransactionByUserIdController receives correctly params

--- src/tests/controllers/transactions/get-transaction-by-userId.test.js
+++ src/tests/controllers/transactions/get-transaction-by-userId.test.js
@@ -23,8 +23,8 @@ describe('GetTransactionById', () => {
     }
 
     const httpRequest = {
+        body: { userId: faker.string.uuid() },
         query: {
-            userId: faker.string.uuid(),
             from: from,
             to: to,
         },
@@ -36,26 +36,6 @@ describe('GetTransactionById', () => {
         const result = await sut.execute(httpRequest)
 
         expect(result.statusCode).toBe(200)
-    })
-
-    it('should returns 400 if is not provided userId', async () => {
-        const { sut } = makeSut()
-
-        const result = await sut.execute({
-            query: { userId: undefined },
-        })
-
-        expect(result.statusCode).toBe(400)
-    })
-
-    it('should returns 400 if provided UserId is invalid', async () => {
-        const { sut } = makeSut()
-
-        const result = await sut.execute({
-            query: { userId: 'Invalid_UserId' },
-        })
-
-        expect(result.statusCode).toBe(400)
     })
 
     it('should returns 404 if GetTransactionsByUserIdUseCase throws generic error', async () => {
@@ -90,7 +70,7 @@ describe('GetTransactionById', () => {
         await sut.execute(httpRequest)
 
         expect(executeSpy).toHaveBeenCalledWith(
-            httpRequest.query.userId,
+            httpRequest.body.userId,
             httpRequest.query.from,
             httpRequest.query.to,
         )
