ea995215e2381679fad70b61e00cc55c4b2d3004
Lucas Defilippo <lucasdefi00@gmail.com>
2026-02-12 23:31:11 -0300

    feat: add /api/transactions/me routes

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -9,7 +9,7 @@ import { auth } from '../middlewares/auth.js'
 
 export const transactionsRoute = Router()
 
-transactionsRoute.get('/', auth, async (request, response) => {
+transactionsRoute.get('/me', auth, async (request, response) => {
     const getTransactionsByUserId = makeGetTransactionsByUserIdController()
 
     const { statusCode, body } = await getTransactionsByUserId.execute({
@@ -25,7 +25,7 @@ transactionsRoute.get('/', auth, async (request, response) => {
     response.status(statusCode).send(body)
 })
 
-transactionsRoute.post('/', auth, async (request, response) => {
+transactionsRoute.post('/me', auth, async (request, response) => {
     const createTransactionController = makeCreateTransactionController()
     const { statusCode, body } = await createTransactionController.execute({
         ...request,
@@ -38,30 +38,38 @@ transactionsRoute.post('/', auth, async (request, response) => {
     response.status(statusCode).send(body)
 })
 
-transactionsRoute.patch('/:transactionId', auth, async (request, response) => {
-    const updateTransactionController = makeUpdateTransactionsController()
+transactionsRoute.patch(
+    '/me/:transactionId',
+    auth,
+    async (request, response) => {
+        const updateTransactionController = makeUpdateTransactionsController()
 
-    const { statusCode, body } = await updateTransactionController.execute({
-        ...request,
-        body: {
-            ...request.body,
-            user_id: request.userId,
-        },
-    })
+        const { statusCode, body } = await updateTransactionController.execute({
+            ...request,
+            body: {
+                ...request.body,
+                user_id: request.userId,
+            },
+        })
 
-    response.status(statusCode).send(body)
-})
+        response.status(statusCode).send(body)
+    },
+)
 
-transactionsRoute.delete('/:transactionId', auth, async (request, response) => {
-    const deleteTransactionController = makeDeleteTransactionController()
+transactionsRoute.delete(
+    '/me/:transactionId',
+    auth,
+    async (request, response) => {
+        const deleteTransactionController = makeDeleteTransactionController()
 
-    const { statusCode, body } = await deleteTransactionController.execute({
-        ...request,
-        body: {
-            ...request.body,
-            user_id: request.userId,
-        },
-    })
+        const { statusCode, body } = await deleteTransactionController.execute({
+            ...request,
+            body: {
+                ...request.body,
+                user_id: request.userId,
+            },
+        })
 
-    response.status(statusCode).send(body)
-})
+        response.status(statusCode).send(body)
+    },
+)
